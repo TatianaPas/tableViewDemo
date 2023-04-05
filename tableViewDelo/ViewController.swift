cbd4d231bfdf6d6379f2d19597155ef579b0947c
Tatiana Pasechnik <tatjana.pasechnik@gmail.com>
2023-04-06 11:31:06 +1200

    Initial Commit

---
+++ tableViewDelo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewDelo
+//
+//  Created by Tatiana Pasechnik on 6/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
